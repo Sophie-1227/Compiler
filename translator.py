f6729c534434fae209819c3f4859247f33c65a48
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2023-01-18 02:40:01 +0100

    Adding Multiplication, division and modulo to lib

--- translator.py
+++ translator.py
@@ -2,7 +2,7 @@ from symbol_table import Variable
 import global_
 #from kompilator import MyLexer, MyParser
 
-line_num = 28 #zaczynamy od 28 bo w pierwszej linijce zawsze będzie skok do main'a, a kolejne to library
+line_num = 62 #zaczynamy od 62 bo w pierwszej linijce zawsze będzie skok do main'a, a kolejne to library
 
 class Translator:
 
@@ -14,28 +14,7 @@ class Translator:
         self.commands = commands
         self.code = []
 
-    def generate_code(self):
-        self.code.append("JUMPI " + str(global_.list_of_variables.index("ma1n 1ump")))
-        for instruction in global_.instructions:
-            block = list(instruction)
-            if block[0] == 'procedure':
-                self.code.append("SET " + str(line_num+1))
-                self.code.append("STORE " + str(global_.list_of_variables.index(str(block[1])+" 1ump")))
-                line_num += 2
-            elif block[0] == 'program':
-                self.code.append("SET " + str(line_num+1))
-                self.code.append("STORE " + str(global_.list_of_variables.index("ma1n 1ump"))) #ewentualnie hard code nazwy funkcji jako main czy coś w tym stylu
-                line_num += 2
-            elif block[0] == 'if':
-                pass
-            elif block[0] == 'ifelse':
-                pass
-
-        self.code.append("HALT")
-
-    #a i b będą przekazywane w funkcji generate_code 
-    def generate_ready_library(self, a, b):
-        pass
+    def generate_ready_library(self):
         #Equal evaluation
         self.code.append("SET 1")
         self.code.append("ADD 3")
@@ -67,8 +46,64 @@ class Translator:
         self.code.append("JPOS 27")
         self.code.append("JUMPI i")
         self.code.append("JUMPI 2")
+        #Multiplication
+        self.code.append("LOAD 4")
+        self.code.append("STORE 6")
+        self.code.append("STORE 7")
+        self.code.append("SET 1")
+        self.code.append("STORE 4")
+        self.code.append("LOAD 36")
+        self.code.append("STORE 2")
+        self.code.append("JUMP 17")
+        self.code.append("LOAD 6")
+        self.code.append("ADD 7")
+        self.code.append("STORE 6")
+        self.code.append("LOAD 4")
+        self.code.append("ADD 1")
+        self.code.append("JUMP 17")
+        #Division
+        self.code.append("SET 1")
+        self.code.append("SUB 1")
+        self.code.append("STORE 6")
+        self.code.append("LOAD 48")
+        self.code.append("STORE 2")
+        self.code.append("JUMP 22")
+        self.code.append("LOAD 3")
+        self.code.append("SUB 4")
+        self.code.append("STORE 3")
+        self.code.append("LOAD 6")
+        self.code.append("ADD 1")
+        self.code.append("STORE 6")
+        self.code.append("JUMP 22")
+        #Modulo
+        self.code.append("LOAD 58")
+        self.code.append("STORE 2")
+        self.code.append("JUMP 22")
+        self.code.append("LOAD 3")
+        self.code.append("SUB 4")
+        self.code.append("STORE 3")
+        self.code.append("JUMP 22")
 
 
+    def generate_code(self):
+        self.code.append("JUMPI " + str(global_.list_of_variables.index("ma1n 1ump")))
+        Translator.generate_ready_library(self)
+        for instruction in global_.instructions:
+            block = list(instruction)
+            if block[0] == 'procedure':
+                self.code.append("SET " + str(line_num+1))
+                self.code.append("STORE " + str(global_.list_of_variables.index(str(block[1])+" 1ump")))
+                line_num += 2
+            elif block[0] == 'program':
+                self.code.append("SET " + str(line_num+1))
+                self.code.append("STORE " + str(global_.list_of_variables.index("ma1n 1ump"))) #ewentualnie hard code nazwy funkcji jako main czy coś w tym stylu
+                line_num += 2
+            elif block[0] == 'if':
+                pass
+            elif block[0] == 'ifelse':
+                pass
+
+        self.code.append("HALT")
 
     def translate_commands(self, commands):
         for command in commands:
