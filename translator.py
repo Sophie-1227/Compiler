bd169be4e06537a9b5c42e186d3b9f2c928d4d92
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2023-01-18 03:46:02 +0100

    First translator idea

--- translator.py
+++ translator.py
@@ -3,6 +3,7 @@ import global_
 #from kompilator import MyLexer, MyParser
 
 line_num = 62 #zaczynamy od 62 bo w pierwszej linijce zawsze będzie skok do main'a, a kolejne to library
+location = None
 
 class Translator:
 
@@ -84,60 +85,85 @@ class Translator:
         self.code.append("STORE 3")
         self.code.append("JUMP 22")
 
-
-    def generate_code(self):
+    def generate_code(self, lista):
         self.code.append("JUMPI " + str(global_.list_of_variables.index("ma1n 1ump")))
         Translator.generate_ready_library(self)
-        for instruction in global_.instructions:
-            block = list(instruction)
+        for block in lista:
             if block[0] == 'procedure':
-                self.code.append("SET " + str(line_num+1))
-                self.code.append("STORE " + str(global_.list_of_variables.index(str(block[1])+" 1ump")))
+                location = block[1]
+                self.code.append("SET " + str(line_num+3))
+                self.code.append("STORE " + str(global_.list_of_variables.index(str(lista[1])+" 1ump")))
                 line_num += 2
+                Translator.generate_code(self, block)
             elif block[0] == 'program':
-                self.code.append("SET " + str(line_num+1))
-                self.code.append("STORE " + str(global_.list_of_variables.index("ma1n 1ump"))) #ewentualnie hard code nazwy funkcji jako main czy coś w tym stylu
+                location = "ma1n"
+                self.code.append("SET " + str(line_num+3))
+                self.code.append("STORE " + str(global_.list_of_variables.index("ma1n 1ump")))
                 line_num += 2
-            elif block[0] == 'if':
-                pass
-            elif block[0] == 'ifelse':
-                pass
+                Translator.generate_code(self, block)
+            elif block[0] == 'proc':
+                location = block[1]
+                self.code.append("SET " + str(line_num+4))
+                self.code.append("STORE 2")
+                self.code.append("JUMPI " + str(global_.list_of_variables.index(str(block[1]) + " 1ump")))
+                line_num += 3
+                Translator.generate_code(self, block)
+            elif block[0] == 'read':
+                self.code.append("GET " + str(global_.list_of_variables.index(str(location) + str(block[1]))))
+                line_num += 1
+                Translator.generate_code(self, block)
+            elif block[0] == 'write':
+                self.code.append("PUT " + str(global_.list_of_variables.index(str(location) + str(block[1]))))
+                line_num += 1
+                Translator.generate_code(self, block)
+            elif block[0] == 'assign':
+                Translator.generate_code(self, block)
+                self.code.append("STORE " + str(global_.list_of_variables.index(str(location) + str(block[1]))))
+                line_num += 1
+            elif block[0] == 'add':
+                self.code.append("LOAD " + str(global_.list_of_variables.index(str(location) + str(block[1]))))
+                self.code.append("ADD " + str(global_.list_of_variables.index(str(location) + str(block[2]))))
+                line_num += 2
+            elif block[0] == 'sub':
+                self.code.append("LOAD " + str(global_.list_of_variables.index(str(location) + str(block[1]))))
+                self.code.append("SUB " + str(global_.list_of_variables.index(str(location) + str(block[2]))))             
 
-        self.code.append("HALT")
 
-    def translate_commands(self, commands):
-        for command in commands:
-            if command[0] == "write":
-                pass
-            elif command[0] == "read":
-                pass
-            elif command[0] == "assign":
-                pass
-            elif command[0] == "if":
-                pass
-            elif command[0] == "ifelse":
-                pass
-            elif command[0] == "while":
-                pass
-            elif command[0] == "repeat":
-                pass
 
-    def calculate_expressions(self, expressions):
-        for expression in expressions:
-            if expression[0] == "constant":
-                pass
-            elif expression[0] == "id":
-                pass
-            elif expression[0] == "add":
-                pass
-            elif expression[0] == "sub":
-                pass
-            elif expression[0] == "mul":
-                pass
-            elif expression[0] == "div":
-                pass
-            elif expression[0] == "mod":
-                pass
 
-    def eval_condition(self, condition):
-        pass
+
+
+
+    # def translate_commands(self, commands):
+    #     for command in commands:
+    #         if command[0] == "write":
+    #             pass
+    #         elif command[0] == "read":
+    #             pass
+    #         elif command[0] == "assign":
+    #             pass
+    #         elif command[0] == "if":
+    #             pass
+    #         elif command[0] == "ifelse":
+    #             pass
+    #         elif command[0] == "while":
+    #             pass
+    #         elif command[0] == "repeat":
+    #             pass
+
+    # def calculate_expressions(self, expressions):
+    #     for expression in expressions:
+    #         if expression[0] == "constant":
+    #             pass
+    #         elif expression[0] == "id":
+    #             pass
+    #         elif expression[0] == "add":
+    #             pass
+    #         elif expression[0] == "sub":
+    #             pass
+    #         elif expression[0] == "mul":
+    #             pass
+    #         elif expression[0] == "div":
+    #             pass
+    #         elif expression[0] == "mod":
+    #             pass
