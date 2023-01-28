31ddfef5958c9b9a38932fc3176b2d579fbffd04
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2023-01-28 19:44:20 +0100

    flag handling

--- flags.py
+++ flags.py
@@ -1,6 +1,10 @@
 import global_
+from translator import Translator
 
 class Flag:
     def flag_catcher():
         code = Translator()
         code.generate_code(global_.instructions)
+        for flag in global_.list_of_variables:
+            if flag in code:
+                pass
